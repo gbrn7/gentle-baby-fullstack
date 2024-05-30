d795ce93f9c720ba3631d85b1b758e4a63a39958
Muhammad Rayhan Gibran <mraygbrn@gmail.com>
2024-05-30 08:58:22 +0700

    feat(x-icon): add x-icon

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -6,7 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta name="csrf-token" content="{{csrf_token()}}" />
   <title>Gentle Baby | Client</title>
-
+  <!-- Icon -->
+  <link rel="icon" type="image/x-icon" href="{{asset('Assets/Img/package.png')}}" />
 
   {{-- Bootrsrap Css --}}
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
